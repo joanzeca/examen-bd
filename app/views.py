474a7aca85dd958da4d69082c8368ab66b7464e0
Joanzeca <joseantoniozc2001@gmail.com>
2024-07-11 13:44:28 -0500

    Estilos a la navegación

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
+from .models import Alumno
 
 # Create your views here.
 
@@ -10,4 +11,9 @@ def ingreso(request):
     return render(request, "ingreso.html")
 
 def listado(request):
-    return render(request, "listado.html")
+    try:
+        alumnos = Alumno.objects.all()
+    except ValueError:
+        raise Http404("Alumnos no encontrados.", ValueError)
+    
+    return render(request, "listado.html", {"alumnos":alumnos})
