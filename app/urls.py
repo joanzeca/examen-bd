d484750267dcb7c74c61648acee3db0f785bbbef
Joanzeca <joseantoniozc2001@gmail.com>
2024-07-11 18:06:34 -0500

    Vista listado terminada

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, ingreso, listado 
+from .views import home, ingreso, listado, api_listado
 
 urlpatterns = [
     path("", home, name="home"),
     path("ingreso/", ingreso, name="ingreso"),
     path("listado/", listado, name="listado"),
+    path("api/listado/", api_listado, name="api_listado"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, JsonResponse
 from .models import Alumno
+from django.core.serializers import serialize
 
 # Create your views here.
 
@@ -14,6 +15,21 @@ def listado(request):
     try:
         alumnos = Alumno.objects.all()
     except ValueError:
-        raise Http404("Alumnos no encontrados.", ValueError)
+        raise Http404("Ha ocurrido un error al hacer la consulta a la tabla Alumno.", ValueError)
     
     return render(request, "listado.html", {"alumnos":alumnos})
+
+def api_listado(request):
+    try:
+        alumnos = Alumno.objects.all().values()
+        alumnos_list = list(alumnos)
+        # Agregar la propiedad 'condicion' a cada alumno
+        for alumno in alumnos_list:
+            alumno_obj = Alumno.objects.get(id=alumno['id'])  # Suponiendo que 'id' es la clave primaria
+            alumno['condicion'] = alumno_obj.condicion
+            alumno['promedio'] = alumno_obj.promedio
+            alumno['fecha_reg'] = alumno_obj.fecha_reg.strftime("%d-%m-%Y")
+    except ValueError:
+        raise Http404("Ha ocurrido un error al hacer la consulta a la tabla Alumno.", ValueError)
+    
+    return JsonResponse(alumnos_list, safe=False)
