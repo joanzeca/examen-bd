ee2f4de786d09a36e0703d185f9a987856fe637a
Joanzeca <joseantoniozc2001@gmail.com>
2024-07-11 12:00:36 -0500

    Modelo Alumno creado y migraciones realizadas

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Alumno(models.Model):
+    nombre = models.CharField(max_length=100)
+    curso = models.CharField(max_length=100)
+    nota1 = models.FloatField()
+    nota2 = models.FloatField()
+    fecha_reg = models.DateTimeField(default=timezone.now)
+    
+    @property
+    def promedio(self):
+        return (self.nota1 + self.nota2) / 2
+
+    @property
+    def condicion(self):
+        return "Aprobó" if self.promedio >= 10.5 else "No aprobó"
+
+    def __str__(self):
+        return self.nombre
